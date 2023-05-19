4623dc82936366c48eab28e474b94b6d965574c8
bsh412 <126067545+bsh412@users.noreply.github.com>
2023-05-19 18:23:03 +0200

    Create individual2.c
    bsh412.c

---
+++ individual2.c
@@ -0,0 +1,55 @@
+1. (30,2)10 =(11110,010)2 (30,2)3 No válido
+(30,2)5=(1111,011)2 (30,2)8=(11000,010)2
+
+2. Base 2: 11101,01/0,11=100110,10 √11101,01=101,01
+Base 16: A8900AC3D-FCD710=A8803D52D F9AC*C8B=C3BA064
+
+3. n=5 Nº máximo de caracteres que se pueden codificar: 2+4+8+16+32=62
+Nº de caracteres a representar=50
+Nota: Morse es un sistema de codificación con un alfabeto binario pero no es un
+sistema de numeración
+
+4.
+Número Representación entera (12 bits)
+Signo y magnitud Complemento a 1 Complemento a 2 Desplazada
++400 0001 1001 0000 0001 1001 0000 0001 1001 0000 1001 1001 0000
+-500 1001 1111 0100 1110 0000 1011 1110 0000 1100 0110 0000 1100
++2048 Fuera de rango en todas las representaciones
+-2048 No se puede representar 1000 0000 0000 0000 0000 0000
+
+5. 7F800001  NaN 80400000  -0,5*2-126
+-1,0485768*106  C9800006
+-∞  FF800000
+Precisión: 2-23*2E=0,1*10-6*2E  aproximadamente las 6 primeras cifras del número son
+exactas
+
+6. x
+2+y2+2*x*y+x+y=(x+y)2+(x+y)
+Instrucción Contenidos de los registros
+A B
+INP x
+CP A,B x x
+INP y x
+ADD y+x x
+CP A,B y+x y+x
+MULT (y+x)2
+y+x
+ADD (y+x)2+(y+x) y+x
+OUT
+
+7. En la Unidad de Control  registro IR (Registro de Instrucción)
+En la Unidad Aritmético-Lógica  banco de registros
+En Memoria Central
+La dirección de memoria central donde está se almacena en el registro PC (Contador de
+Programa)
+
+8. MC máxima  4 Gpalabras=16 GB
+Ancho de banda  3,7 GB/sg
+5 sg  18,6 GB
+
+9. Si, pero traduciéndolo previamente al lenguaje máquina con el correspondiente traductor
+No, solamente se puede ejecutar en computadoras con arquitectura compatible
+
+(diferentes arquitecturas usan lenguajes máquina distintos y no compatibles entre sí)
+10. DIR4\examen.doc \DIR2\DIR5\DIR4\examen.doc DIR2\DIR5\DIR4\examen.doc
+Sí, siempre que estén en directorios diferentes
