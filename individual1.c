d5c82edee887a15031ebf2ced61f505450b7b5e4
bsh412 <126067545+bsh412@users.noreply.github.com>
2023-05-19 18:22:15 +0200

    Create individual1.c
    bsh412.c

---
+++ individual1.c
@@ -0,0 +1,93 @@
+1) 32 KB = 262 144 bits
+64 MB = 536 870 912 bits
+4 GB = 34 359 738 368 bits
+
+2) 147 456 caracteres (bytes)
+
+3) (10110)2 = (22)10 (0,10011)2 = (0,59375)10
+(7354)8 = (3820)10 (0,70612)8 = (0,88702392578125)10
+(F7398)16 = (1 012 632)10 (0,758FA)16 = (0,4592227935791015625)10
+
+4) (525)10 = (1 000 001 101)2 (0,17)10 = (0,0010101)2
+(372)8 = (11 111 010)2 (0,0375)8 = (0,000 011 111 101)2
+(37F)16 = (11 0111 1111)2 (0,0AC54)16 = (0,0000 1010 1100 0101 0100)2
+
+5) 10011 + 11011 = 101110
+11100 – 10001 = 1011
+1101 * 101 = 1 000 001
+1101 / 101 = 10,100
+(1010,1001)1/2 = 11,01
+
+6) Base 8: 664 + 757,3 = 1643,3
+375 – 177 = 176
+453 * 3 = 1601
+Base 16: A8900GC30 – FFD710 (no válida  G?)
+3F68 + 43B = 43A3
+FA76 * CBA = C7381BC
+
+7) Números decimales: 9375 C10 = 0625
+3798 C9 = 6201
+Números binarios: 1 011 111 000 C2 = 0 100 001 000
+1 101 111 111 101 C1 = 0 010 000 000 010
+
+8)
+Sist. numeración Decimal Binario Octal Hexadecimal
+Resta 00001748
+– 00000547
+00111111
+– 00101111
+00001743
+– 00000547
+0000F8A4
+– 0000C9AA
+Directa 00001201 00010000 00001174 00002EFA
++CB-1(sustraendo) 00001200 00001111 00001173 00002EF9
++CB(sustraendo) 00001201 00010000 00001174 00002EFA
+
+9) 7 bits
+
+10) Unicode  16 bits  65536 caracteres diferentes
+'K'  (75)10=(4B)16  Unicode: 004B
+'Ñ'  (209)10=(D1)16  Unicode: 00D1
+
+11) 1,2,3
+ESTO es una prueba.
+
+12) Son códigos de control de comunicaciones (telégrafo) y de impresora:
+SOH: Start Of Heading
+STX: Start Of Text
+ETX: End Of Text
+ENQ: ENQuiry
+ACK: ACKnowledge
+BEL: BELL
+BS: Back Space
+Programación Trabajo individual 1.2
+
+
+13)
++100 -127 +255 -128
+Signo y magnitud 0110 0100 1111 1111 No es posible No es posible
+Complemento a 1 0110 0100 1000 0000 No es posible No es posible
+Complemento a 2 0110 0100 1000 0001 No es posible 1000 0000
+Desplazada 1110 0100 0000 0001 No es posible 0000 0000
+
+14)
+1010 1110 0111 1011 1000 0000
+Sin signo 174 123 128
+Signo y magnitud -46 +123 -0
+Complemento a 1 -81 +123 -127
+Complemento a 2 -82 +123 -128
+Desplazada 46 -5 0
+BCD No válida No válida 80
+
+15) BC7E0000  -0.015502929
+80000004  -5.6051938 * 10-45
+FF800000  -∞
+7FF80700  +NaN
+
+16) 7  40E0 0000
+-7.5 * 2-140  8000 7800
+
+17) Máximo: 1.7977 * 10308
+Mínimo: Normalizado: 2.2251 * 10-308
+De-normalizado: 4.9407 * 10-324
